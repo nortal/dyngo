ad20fde1fabee7dbd93487cb047aeaf0510ca75e
Vassili Jakovlev <vassili.jakovlev@gmail.com>
2015-12-09 23:50:19 +0200

    added build-time environment configuration for demo

--- demo/src/app/demo/demo.js
+++ demo/src/app/demo/demo.js
@@ -5,11 +5,11 @@
 
     .service('DemoService', function($resource, config) {
       this.loadStructure = function(formName) {
-        return $resource(config.formUrl).get({formName: formName}).$promise;
+        return $resource(config.loadFormUrl).get({formName: formName}).$promise;
       };
 
       this.saveData = function(data) {
-        return $resource(config.submitUrl).save(data).$promise;
+        return $resource(config.saveDataUrl).save(data).$promise;
       };
     })
 
