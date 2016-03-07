989a5811f66f65d1812cbc1aef5220661d92a04e
Vassili Jakovlev <vassili.jakovlev@gmail.com>
2016-03-07 13:49:26 +0200

    angular version restricted to 1.4.x

--- dist/dyngo.js
+++ dist/dyngo.js
@@ -238,49 +238,6 @@ angular.module('dyngo.component.provider', [])
     };
   });
 
-angular.module('dyngo.container', [])
-
-  .controller('ContainerCtrl', ["$scope", function ($scope) {
-    $scope.visible = function (component) {
-      var visible = true;
-
-      var unsetData = function (component) {
-        delete $scope.data[component.id];
-        angular.forEach(component.components, function (child) {
-          unsetData(child);
-        });
-      };
-
-      var visibilityExpression = component.constraints ? component.constraints.visible : undefined;
-      if (visible && angular.isDefined(visibilityExpression)) {
-        visible = $scope.$eval(visibilityExpression, $scope.data);
-      }
-      if (!visible) {
-        unsetData(component);
-      }
-      return visible;
-    };
-
-  }])
-
-  .directive('dgContainer', function () {
-    return {
-      restrict: 'A',
-      require: 'ngModel',
-      scope: {
-        container: '=dgContainer',
-        data: '=ngModel'
-      },
-      template: '<div ng-repeat="component in container.components" dg-component="component" ng-model="data" ng-if="visible(component)"></div>',
-      controller: 'ContainerCtrl',
-      link: function (scope) {
-        scope.formModel = scope.$parent.formModel;
-        scope.formName = scope.$parent.formName;
-        scope.lang = scope.$parent.lang;
-      }
-    };
-  });
-
 angular.module('dyngo.form', ['dyngo.translator'])
 
   .provider('dyngo', function () {
@@ -325,6 +282,54 @@ angular.module('dyngo.form', ['dyngo.translator'])
       },
       controller: 'FormCtrl',
       template: '<div dg-container="form.structure" ng-model="data" ng-if="form"></div>'
+    };
+  });
+
+angular.module('dyngo.container', [])
+
+  .controller('ContainerCtrl', ["$scope", function($scope) {
+    $scope.visible = function(component) {
+
+      var unsetData = function(component) {
+        delete $scope.data[component.id];
+        angular.forEach(component.components, function(child) {
+          unsetData(child);
+        });
+      };
+
+      var visible = evalVisibilityExpression(component);
+
+      if (!visible) {
+        unsetData(component);
+      }
+      return visible;
+    };
+
+    function evalVisibilityExpression(component) {
+      var visibilityExpression = component.constraints ? component.constraints.visible : undefined;
+      if (angular.isDefined(visibilityExpression)) {
+        return $scope.$eval(visibilityExpression, $scope.data);
+      }
+      return true;
+    }
+
+  }])
+
+  .directive('dgContainer', function() {
+    return {
+      restrict: 'A',
+      require: 'ngModel',
+      scope: {
+        container: '=dgContainer',
+        data: '=ngModel'
+      },
+      template: '<div ng-repeat="component in container.components" dg-component="component" ng-model="data" ng-if="visible(component)"></div>',
+      controller: 'ContainerCtrl',
+      link: function(scope) {
+        scope.formModel = scope.$parent.formModel;
+        scope.formName = scope.$parent.formName;
+        scope.lang = scope.$parent.lang;
+      }
     };
   });
 
